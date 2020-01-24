876e45b8d42b6ea419bde584c2276c3cfd02f2d2
caitlynschurter <33267088+caitlynschurter@users.noreply.github.com>
2020-01-23 16:28:53 -0800

    Add files via upload

---
+++ src/GameObject.java
@@ -0,0 +1,24 @@
+
+//Evil Lily's Password: JesusChrist123!
+import java.awt.Graphics;
+
+public class GameObject {
+	int _x;
+	int _y;
+	int _width;
+	int _height;
+
+	GameObject(int x, int y, int width, int height) {
+		_x = x;
+		_y = y;
+		_width = width;
+		_height = height;
+	}
+
+	public void update() {
+	}
+
+	public void draw(Graphics g) {
+		g.fillRect(_x, _y, 120, 80);
+	}
+}
