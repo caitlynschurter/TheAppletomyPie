460f6b9df97e1d78f3cdc626d2fb322d66c8adbc
caitlynschurter <33267088+caitlynschurter@users.noreply.github.com>
2020-01-23 16:30:24 -0800

    Updated

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,5 +1,7 @@
 
 //Evil Lily's Password: JesusChrist123!
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,44 +11,115 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class GamePanel extends JPanel implements ActionListener, KeyListener {
-	Timer timer;
+	Timer timer;;
+
+	final int MENU_STATE = 0;
+	final int GAME_STATE = 1;
+	final int END_STATE = 2;
+	int currentState = MENU_STATE;
+
+	Font titleFont;
+	Font titleFontEnter;
+	Font titleFontSpace;
+
+	Font gameOverFont;
+	Font gameOverFontKills;
+	Font gameOverFontRestart;
 
 	public GamePanel() {
 		timer = new Timer(1000 / 60, this);
+		
+		titleFont = new Font("Arial", Font.PLAIN, 48);
+		titleFontEnter = new Font("Arial", Font.PLAIN, 36);
+		titleFontSpace = new Font("Arial", Font.PLAIN, 36);
 	}
 
 	void startGame() {
 		timer.start();
 	}
 
-	@Override
+	void updateMenuState() {
+	}
 
-	public void paintComponent(Graphics g) {
-		g.fillRect(10, 10, 120, 80);
+	void updateGameState() {
+	}
+
+	void updateEndState() {
+	}
+
+	void drawMenuState(Graphics g) {
+		g.setColor(Color.BLUE);
+		g.fillRect(0, 0, TheAppleToMyPie._width, TheAppleToMyPie._height);
+		g.setFont(titleFont);
+	}
+
+	void drawGameState(Graphics g) {
+		g.setColor(Color.CYAN);
+		g.fillRect(0, 0, TheAppleToMyPie._width, TheAppleToMyPie._height);
+
+	}
+
+	void drawEndState(Graphics g) {
+		g.setColor(Color.PINK);
+		g.fillRect(0, 0, TheAppleToMyPie._width, TheAppleToMyPie._height);
+
 	}
 
 	@Override
-	public void keyPressed(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		System.out.println("keyPressed");
+	public void paintComponent(Graphics g) {
+		switch (currentState) {
+		case MENU_STATE:
+			drawMenuState(g);
+			break;
+		case GAME_STATE:
+			drawGameState(g);
+			break;
+		case END_STATE:
+			drawEndState(g);
+			break;
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// System.out.println("keyPressed");
+		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+
+			if (currentState == END_STATE) {
+				//use for later for Game Over
+			}
+
+			currentState++;
+		}
+		if (currentState > END_STATE) {
+			currentState = MENU_STATE;
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
 		System.out.println("keyReleased");
 	}
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
 		System.out.println("keyTyped");
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// System.out.println("hey there");
 		repaint();
+		switch (currentState) {
+		case MENU_STATE:
+			updateMenuState();
+			break;
+		case GAME_STATE:
+			updateGameState();
+			break;
+		case END_STATE:
+			updateEndState();
+			break;
+		}
 
 	}
 
