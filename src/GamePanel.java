51d0dbab9554a93118418803c96b9eac5233c1aa
caitl <caitl@SurfaceKitty6>
2020-03-19 16:37:58 -0700

    TheAppleToMyPie Instruction Page   3/19/20

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -25,6 +25,13 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	Font titleFontEnter;
 	Font titleFontSpace;
 
+	Font instFontTOP;
+	Font instFontTOP2;
+	Font instFontMID;
+	Font instFontMID2;
+	Font instFontBOTTOM;
+	Font instFontBOTTOM2;
+
 	Font scoreFont;
 	Font appleFont;
 	Font flourFont;
@@ -46,6 +53,13 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 		titleFont = new Font("Freestyle Script", Font.PLAIN, 72);
 		titleFontEnter = new Font("Freestyle Script", Font.PLAIN, 48);
 		titleFontSpace = new Font("Freestyle Script", Font.PLAIN, 48);
+
+		instFontTOP = new Font("Courier NEw", Font.BOLD, 24);
+		instFontMID = new Font("Courier NEw", Font.BOLD, 24);
+		instFontBOTTOM = new Font("Courier NEw", Font.BOLD, 24);
+		instFontTOP2 = new Font("Courier NEw", Font.BOLD, 24);
+		instFontMID2 = new Font("Courier NEw", Font.BOLD, 24);
+		instFontBOTTOM2 = new Font("Courier NEw", Font.BOLD, 24);
 
 		scoreFont = new Font("Courier New", Font.PLAIN, 28);
 
@@ -85,24 +99,68 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 
 	void drawMenuState(Graphics g) {
 		g.setColor(Color.BLUE);
-		g.fillRect(0, 0, TheAppleToMyPie.width, TheAppleToMyPie.height);
-		g.setColor(Color.WHITE);
-		g.setFont(titleFont);
-		int sw = g.getFontMetrics().stringWidth("The Apple To My Pie");
-		int sx = TheAppleToMyPie.width / 2 - sw / 2;
-		g.drawString("The Apple To My Pie", sx, 150);
-		g.setFont(titleFontEnter);
-		sw = g.getFontMetrics().stringWidth("Press ENTER to Start!");
-		sx = TheAppleToMyPie.width / 2 - sw / 2;
-		g.drawString("Press ENTER to Start!", sx, 400);
-		g.setFont(titleFontSpace);
-		sw = g.getFontMetrics().stringWidth("Press SPACE for Instructions");
-		sx = TheAppleToMyPie.width / 2 - sw / 2;
-		g.drawString("Press SPACE for Instructions", sx, 500);
+		if (INST_STATE == true) {
+			drawInstructions(g);
+		}
+
+		else {
+			g.fillRect(0, 0, TheAppleToMyPie.width, TheAppleToMyPie.height);
+			g.setColor(Color.WHITE);
+			g.setFont(titleFont);
+			int sw = g.getFontMetrics().stringWidth("The Apple To My Pie");
+			int sx = TheAppleToMyPie.width / 2 - sw / 2;
+			g.drawString("The Apple To My Pie", sx, 150);
+			g.setFont(titleFontEnter);
+			sw = g.getFontMetrics().stringWidth("Press ENTER to Start!");
+			sx = TheAppleToMyPie.width / 2 - sw / 2;
+			g.drawString("Press ENTER to Start!", sx, 400);
+			g.setFont(titleFontSpace);
+			sw = g.getFontMetrics().stringWidth("Press SPACE for Instructions");
+			sx = TheAppleToMyPie.width / 2 - sw / 2;
+			g.drawString("Press SPACE for Instructions", sx, 500);
+		}
 	}
 
-	void drawInstructions() {
+	void drawInstructions(Graphics g) {
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, TheAppleToMyPie.width, TheAppleToMyPie.height);
+		g.setColor(Color.BLACK);
 
+		// Use the LEFT and RIGHT
+		g.setFont(instFontTOP);
+		int sw = g.getFontMetrics().stringWidth("Use the LEFT and RIGHT");
+		int sx = TheAppleToMyPie.width / 2 - sw / 2;
+		g.drawString("Use the LEFT and RIGHT", sx, 100);
+
+		// arrow keys to catch ingredients
+		g.setFont(instFontTOP2);
+		sw = g.getFontMetrics().stringWidth("arrow keys to catch ingredients");
+		sx = TheAppleToMyPie.width / 2 - sw / 2;
+		g.drawString("arrow keys to catch ingredients", sx, 140);
+
+		// Finish the recipes,
+		g.setFont(instFontMID);
+		sw = g.getFontMetrics().stringWidth("Finish the recipes,");
+		sx = TheAppleToMyPie.width / 2 - sw / 2;
+		g.drawString("Finish the recipes,", sx, 200);
+
+		// but don't overstock on ingredients
+		g.setFont(instFontMID2);
+		sw = g.getFontMetrics().stringWidth("but don't overstock on ingredients");
+		sx = TheAppleToMyPie.width / 2 - sw / 2;
+		g.drawString("but don't overstock on ingredients", sx, 240);
+
+		// You have 60 seconds, but you can
+		g.setFont(instFontBOTTOM);
+		sw = g.getFontMetrics().stringWidth("You have 60 seconds, but you can");
+		sx = TheAppleToMyPie.width / 2 - sw / 2;
+		g.drawString("You have 60 seconds, but you can", sx, 300);
+
+		// earn back time by completing recipes
+		g.setFont(instFontBOTTOM2);
+		sw = g.getFontMetrics().stringWidth("earn back time by completeing recipes");
+		sx = TheAppleToMyPie.width / 2 - sw / 2;
+		g.drawString("earn back time by completeing recipes", sx, 340);
 	}
 
 	void drawGameState(Graphics g) {
@@ -219,21 +277,24 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 		if (currentState > END_STATE) {
 			currentState = MENU_STATE;
 		}
-	}
-	/*if (currentState == MENU_STATE) {
-	if (code == KeyEvent.VK_SPACE) {
-		if(INST_STATE = false) {
-			INST_STATE = true;
+
+		if (currentState == MENU_STATE) {
+			if (code == KeyEvent.VK_SPACE) {
+				if (INST_STATE == false) {
+					INST_STATE = true;
+
+					System.out.println("Show Instructions");
+				}
+
+				else if (INST_STATE == true) {
+					INST_STATE = false;
+					System.out.println("Hide Instructions");
+				}
+
+			}
 		}
-		
-		else {
-			INST_STATE = false;
-		}
-		
 	}
-}
-}
-*/
+
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// System.out.println("keyReleased");
